085fb352bbadb88935e1d1d479bc70f2ed7d80a7
collegeprogram <nitishsamrat577@gmail.com>
2026-02-26 16:04:53 +0530

    my day4

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-function App(){
+import { useEffect, useState } from "react"
+
+export default function App(){
+  const [count,setCount] = useState(0);
+  const [start,setStart] = useState(false);
+
+  const handle = ()=>{
+    setStart(true);
+  }
+
+  useEffect(()=>{
+
+    if(!start) return;
+    const time = setInterval(()=>{
+      setCount(count=> count+1);
+    },1000);
+
+    return () => clearInterval(time);
+  },[start])
   return(
     <>
-        <h1>Hello World</h1>
-        <p></p>
+    <h1>Day 4</h1>
+    <h1>Count {count}</h1>
+    <br></br>
+    <button onClick={handle}>Increment</button>
     </>
   )
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,7 @@
+ export default function Header(props){
+    return(
+        <>
+        <h1>Hello My name is {props.name} i am {props.age} year old and i live in {props.address}.</h1>
+        </>
+    )
+}
